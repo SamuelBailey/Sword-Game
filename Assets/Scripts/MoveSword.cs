0795618b875b050e595bc5875c75cc1187a9da72
DuckbilledWalrus <cern1999.sb@gmail.com>
2018-01-12 00:52:14 +0000

    Created moving character with sword that rotates around based on mouse input

---
+++ Assets/Scripts/MoveSword.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveSword : MonoBehaviour
+{
+    public Transform playerPosition;
+    public float swordSpeed;
+    Rigidbody swordRigidbody;
+    Vector3 playerOffset;
+
+    private void Start()
+    {
+        swordRigidbody = gameObject.GetComponent<Rigidbody>();
+        playerOffset = gameObject.transform.position - playerPosition.position;
+    }
+
+    private void FixedUpdate()
+    {
+        Quaternion.Euler(Input.GetAxisRaw("Mouse Y"), Input.GetAxisRaw("Mouse X"), 0);
+        transform.RotateAround(playerPosition.position, Vector3.up, Input.GetAxisRaw("Mouse X") * swordSpeed);
+        transform.RotateAround(playerPosition.position, Vector3.right, Input.GetAxisRaw("Mouse Y") * swordSpeed * -1);
+        //Debug.Log(Vector3.Angle())
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed;
+    public float acceleration;
+    Rigidbody playerRigidbody;
+
+    private void Start()
+    {
+        playerRigidbody = gameObject.GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        //Lateral movement
+        Vector3 targetVelocity = new Vector3();
+        targetVelocity.x = Input.GetAxisRaw("Horizontal");  // sideways movement
+        targetVelocity.z = Input.GetAxisRaw("Vertical");    // forwards/backwards movement
+        targetVelocity = targetVelocity.normalized;
+        targetVelocity *= moveSpeed;
+        playerRigidbody.AddForce(findMovementForce(playerRigidbody.velocity, targetVelocity, acceleration));
+    }
+
+    Vector3 findMovementForce(Vector3 currentVelocity, Vector3 targetVelocity, float acceleration)
+    {
+        currentVelocity.y = 0;
+        return (targetVelocity - currentVelocity) * acceleration;
+    }
+}
