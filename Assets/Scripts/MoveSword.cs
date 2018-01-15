4a3ad0516237dbe2f366331fba9054c9f5186972
DuckbilledWalrus <cern1999.sb@gmail.com>
2018-01-15 18:47:42 +0000

    Smoothed sword movement. Limited max rotation of sword

--- Assets/Scripts/MoveSword.cs
+++ Assets/Scripts/MoveSword.cs
@@ -7,19 +7,63 @@ public class MoveSword : MonoBehaviour
     public Transform playerPosition;
     public float swordSpeed;
     Rigidbody swordRigidbody;
-    Vector3 playerOffset;
+    public GameObject player;
+    Vector3 swordOrigin;
+    public float swordDistance;
+    Vector3 targetAngle;
+    public float maxSwordAngle;
+    public int smoothing;
+    public float sensitivity;
 
     private void Start()
     {
         swordRigidbody = gameObject.GetComponent<Rigidbody>();
-        playerOffset = gameObject.transform.position - playerPosition.position;
+        swordRigidbody.velocity = new Vector3(0, 0, 0);
+        //swordOrigin = new Vector3(0, 0, swordDistance);
+        swordOrigin = swordRigidbody.transform.position - player.transform.position;
+
+        targetAngle = new Vector3(0.0f, 0.0f);
     }
+
+    Vector3 lastPosition = new Vector3(0, 0, 0);
 
     private void FixedUpdate()
     {
-        Quaternion.Euler(Input.GetAxisRaw("Mouse Y"), Input.GetAxisRaw("Mouse X"), 0);
-        transform.RotateAround(playerPosition.position, Vector3.up, Input.GetAxisRaw("Mouse X") * swordSpeed);
-        transform.RotateAround(playerPosition.position, Vector3.right, Input.GetAxisRaw("Mouse Y") * swordSpeed * -1);
-        //Debug.Log(Vector3.Angle())
+        targetAngle.x += Mathf.Clamp(Input.GetAxis("Mouse X"), -100, 100) * sensitivity;
+        targetAngle.y += Mathf.Clamp(Input.GetAxis("Mouse Y"), -100, 100) * sensitivity;
+        if (Vector3.Magnitude(targetAngle) > maxSwordAngle)
+            targetAngle = targetAngle.normalized * maxSwordAngle;
+
+        // find sword current angle
+        float xAngle = Vector3.Angle(
+            new Vector3(swordOrigin.x, 0.0f, swordOrigin.z), 
+            new Vector3(swordRigidbody.transform.position.x, 0.0f, swordRigidbody.transform.position.z)
+        );
+        float yAngle = Vector3.Angle(
+            new Vector3(0.0f, swordOrigin.y, swordOrigin.z), 
+            new Vector3(0.0f, swordRigidbody.transform.position.y, swordRigidbody.transform.position.z)
+        );
+        Vector3 swordAngle = new Vector3(xAngle, yAngle);
+        // find the amount and in which direction to move the sword
+        Vector3 directionAngle = Vector3.Lerp(swordAngle, targetAngle, 0.5f);
+
+        lastPosition = (lastPosition * smoothing + targetAngle) / (smoothing + 1);
+
+
+        Vector3 direction = new Vector3();
+        gameObject.transform.rotation = player.transform.rotation;
+        gameObject.transform.position = player.transform.position;
+        gameObject.transform.localPosition = new Vector3(0, 0, 1);
+        gameObject.transform.RotateAround(player.transform.position, Vector3.up, lastPosition.x);
+        gameObject.transform.RotateAround(player.transform.position, Vector3.left, lastPosition.y);
+        
+        Debug.Log(targetAngle);
+    }
+
+    void RotateRigidBodyAboutPoint(Rigidbody rb, Vector3 origin, Vector3 axis, float angle)
+    {
+        Quaternion q = Quaternion.AngleAxis(angle, axis);
+        rb.MovePosition(q * (rb.transform.position - origin) + origin);
+        rb.MoveRotation(rb.transform.rotation * q);
     }
 }
